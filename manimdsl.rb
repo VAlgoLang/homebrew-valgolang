f379088425d5d1461c4aef815a13346092fc1212
Rini Banerjee <26858592+rbanerjee20@users.noreply.github.com>
2020-12-29 18:29:19 +0000

    Updated for java

--- manimdsl.rb
+++ manimdsl.rb
@@ -1,8 +1,8 @@
 class Manimdsl < Formula
   desc "A command line tool to animate data structures using Manim"
   homepage "https://github.com/ManimDSL/ManimDSLCompiler"
-  url "https://github.com/ManimDSL/ManimDSLCompiler/releases/download/latest/compiler.jar"
-  sha256 "2e826dd914a2526d5e585c151b80f2907a7f1d3841c310593109174553d007b3"
+  url "https://github.com/ManimDSL/ManimDSLCompiler/releases/download/latest/brew_zip.zip"
+  sha256 "6705245f5bbb7e54d54eeba0eb9605f70e94924627abef2f079e965a64f49424"
   version "latest"
 
   depends_on "cairo"
@@ -14,7 +14,9 @@ class Manimdsl < Formula
 
   def install
     system "/usr/local/bin/brew", "install", "--cask", "mactex"
-#     system "/usr/local/bin/pip3", "install", "manimlib"
-    bin.install "compiler.jar"
+#     system "/usr/local/bin/pip3", "install", "manimlib"    
+    inreplace "brew/manimdsl", "##PREFIX##", "#{prefix}"
+    prefix.install "compiler.jar"
+    bin.install "brew/manimdsl"
   end
 end
