1507e9332270d57ef8d86b039caa66380a3cde3f
Rini Banerjee <26858592+rbanerjee20@users.noreply.github.com>
2021-01-11 13:04:00 +0000

    Update and rename manimdsl.rb to valgolang.rb

--- valgolang.rb
+++ valgolang.rb
@@ -1,8 +1,8 @@
-class Manimdsl < Formula
+class Valgolang < Formula
   desc "A command line tool to animate data structures using Manim"
-  homepage "https://github.com/ManimDSL/ManimDSLCompiler"
-  url "https://github.com/ManimDSL/ManimDSLCompiler/releases/download/latest/brew_zip.zip"
-  sha256 "a3a750087f80be7ebe99f4350d8f9262a8bd004b1f86af89aed0711dd3329850"
+  homepage "https://github.com/VAlgoLang/VAlgoLang"
+  url "https://github.com/VAlgoLang/VAlgoLang/releases/download/latest/brew_zip.zip"
+  sha256 "3269dbc308a7fd505da28dead1105100d6a9e14ed432c0bf59dfe583eb3d165f"
   version "latest"
 
   depends_on "cairo"
@@ -15,8 +15,8 @@ class Manimdsl < Formula
   def install
     system "/usr/local/bin/brew", "install", "--cask", "mactex"
     system "/usr/local/bin/pip3", "install", "manimlib"    
-    inreplace "manimdsl", "##PREFIX##", "#{prefix}"
+    inreplace "valgolang", "##PREFIX##", "#{prefix}"
     prefix.install "compiler.jar"
-    bin.install "manimdsl"
+    bin.install "valgolang"
   end
 end
