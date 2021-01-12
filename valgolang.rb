6acc2725afd6753327d8cc2a5e1841581f5431f7
Rini Banerjee <26858592+rbanerjee20@users.noreply.github.com>
2021-01-12 23:40:11 +0000

    Update valgolang.rb

--- valgolang.rb
+++ valgolang.rb
@@ -2,7 +2,7 @@ class Valgolang < Formula
   desc "A command line tool to animate data structures using Manim"
   homepage "https://github.com/VAlgoLang/VAlgoLang"
   url "https://github.com/VAlgoLang/VAlgoLang/releases/download/latest/brew_zip.zip"
-  sha256 "3269dbc308a7fd505da28dead1105100d6a9e14ed432c0bf59dfe583eb3d165f"
+  sha256 "96665c779f06bed2cd9d30d8a2070be13df0ce8efcdf73f3b1d843d0ed287cb0"
   version "latest"
 
   depends_on "cairo"
@@ -15,8 +15,8 @@ class Valgolang < Formula
   def install
     system "/usr/local/bin/brew", "install", "--cask", "mactex"
     system "/usr/local/bin/pip3", "install", "manimlib"    
-    inreplace "manimdsl", "##PREFIX##", "#{prefix}"
+    inreplace "valgolang", "##PREFIX##", "#{prefix}"
     prefix.install "compiler.jar"
-    bin.install "manimdsl"
+    bin.install "valgolang"
   end
 end
